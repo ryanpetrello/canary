e839c008aa4c529bda873e6ab2c2c26b2fa40950
Ryan Petrello <lists@ryanpetrello.com>
2013-02-11 16:45:27 -0500

    Add the traceback to the message.

--- lookout/handler.py
+++ lookout/handler.py
@@ -2,7 +2,9 @@ import json
 import logging
 import socket
 import threading
+import traceback
 from datetime import datetime
+
 
 import zmq
 
@@ -11,7 +13,6 @@ class ZeroMQHandler(logging.Handler):
 
     def __init__(self, address="tcp://127.0.0.1:2120", context=None):
         super(ZeroMQHandler, self).__init__()
-        import pdb; pdb.set_trace()
         self._address = address
         self._context = context
         if context is None:
@@ -30,10 +31,18 @@ class ZeroMQHandler(logging.Handler):
         self.publisher.send_unicode(json.dumps(data))
 
     def emit(self, record):
+        exc_info = record.exc_info
+
+        fields = {
+            'traceback': ''.join(traceback.format_exception(*exc_info)),
+        }
+        fields.update(
+            getattr(record, 'fields', {})
+        )
         data = {
             '@timestamp': datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
             '@source_host': socket.gethostname(),
-            '@message': record.msg,
-            '@fields': []
+            '@message': str(exc_info[0]),
+            '@fields': fields
         }
         self._send(data)
