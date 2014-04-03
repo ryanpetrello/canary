febec87fa8babbfbde41faf8846b3db14951eba8
Ryan Petrello <lists@ryanpetrello.com>
2014-04-02 19:10:14 -0700

    Fix broken tests in pyzmq>14

--- canary/tests/__init__.py
+++ canary/tests/__init__.py
@@ -10,13 +10,7 @@ class ListeningTest(unittest.TestCase):
     def setUp(self):
         self.context = zmq.Context.instance()
 
-        self.listener = zmq.Socket(self.context, zmq.SUB)
-        self.listener.setsockopt(zmq.LINGER, 0)
-        self.listener.setsockopt(
-            zmq.SUBSCRIBE,
-            zmq.utils.strtypes.asbytes('')
-        )
-
+        self.listener = zmq.Socket(self.context, zmq.PULL)
         self.port = self.listener.bind_to_random_port("tcp://127.0.0.1")
         self.handler = ZeroMQHandler(
             "tcp://127.0.0.1:{0}".format(self.port),
